446666124ea329a20297d102d358047411c4b929
MateusNogueiraElias <mateusvilacom734@gmail.com>
2025-06-25 22:46:30 -0300

    mudança no index

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './fonts.css' // ADICIONE ESTA LINHA
-
 createRoot(document.getElementById("root")!).render(<App />);
